7e8e0508c67c0c5ddc7c51e71a8a6f0c685e53d0
Keiren-b <keiren_b@hotmail.com>
2022-03-28 21:52:48 +0200

    update

---
+++ src/DomElements.js
@@ -0,0 +1,100 @@
+const content = document.getElementById('content')
+
+
+function createContainers(){
+inputContainer = document.createElement('div')
+inputContainer.setAttribute('id', 'inputContainer')
+
+const cardContainer = document.createElement('div')
+cardContainer.setAttribute('id', 'cardContainer')
+
+}
+
+function createButtons(){
+    const NewToDoBtn = document.createElement('button')
+    NewToDoBtn.innerHTML = 'Add New To-Do'
+    content.appendChild(NewToDoBtn)
+
+    const NewProjectBtn = document.createElement('button')
+    NewProjectBtn.innerHTML = 'Create New Project'
+    content.appendChild(NewProjectBtn)
+}
+
+function createinputBoxes(){
+
+    // render.hide(inputContainer)
+  
+      const title = document.createElement("input");
+      title.name = "Title";
+      title.setAttribute('id', 'title')
+      const titlelabel = document.createElement("Label");
+      titlelabel.setAttribute("for", title);
+      titlelabel.innerHTML = "Title: ";
+      inputContainer.appendChild(titlelabel);
+      inputContainer.appendChild(title)
+  
+      const desc = document.createElement("textarea");
+      desc.name = "Description"
+      desc.setAttribute('id', 'desc')
+      const desclabel = document.createElement("Label");
+      desclabel.setAttribute("for", desc);
+      desclabel.innerHTML = "Description: ";
+      inputContainer.appendChild(desclabel);
+      inputContainer.appendChild(desc)
+  
+      const due = document.createElement("input");
+      due.type = 'date'
+      due.name = "Due"
+      due.setAttribute('id', 'due')
+      const duelabel = document.createElement("Label");
+      duelabel.setAttribute("for", due);
+      duelabel.innerHTML = "Due Date: ";
+      inputContainer.appendChild(duelabel);
+      inputContainer.appendChild(due)
+  
+      const priority = document.createElement("select");
+      priority.name = "Priority"
+      priority.setAttribute('id', 'priority')
+      let values = ['Low', 'Medium', 'High', 'Ugent']
+      for (const val of values)
+      {           var option = document.createElement("option");
+                  option.value = val;
+                  option.text = val.charAt(0).toUpperCase() + val.slice(1);
+                  priority.appendChild(option);
+              }
+      const prioritylabel = document.createElement("Label");
+      prioritylabel.setAttribute("for", priority);
+      prioritylabel.innerHTML = "Priority: ";
+      inputContainer.appendChild(prioritylabel);
+      inputContainer.appendChild(priority)
+  
+         
+  
+      const notes = document.createElement("textarea");
+  
+      notes.name = "Notes"
+      notes.setAttribute('id', 'notes')
+      const noteslabel = document.createElement("Label");
+      noteslabel.setAttribute("for", priority);
+      noteslabel.innerHTML = "Notes: ";
+      inputContainer.appendChild(noteslabel);
+      inputContainer.appendChild(notes)
+  
+      const submitBtn = document.createElement('button')
+      submitBtn.textContent = "Add To-Do"
+      submitBtn.setAttribute('id', 'printBtn')
+      inputContainer.appendChild(submitBtn)
+  
+      content.appendChild(inputContainer)
+  
+   
+  
+      
+       
+        }
+    
+
+
+        createContainers()
+        createButtons()
+        createinputBoxes()
